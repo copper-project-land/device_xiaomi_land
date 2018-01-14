1dc211f2e51d18f78a5adcf9ecb37749ef5c77bc
Devil7 <dineshthangavel47@gmail.com>
2018-01-14 10:32:43 +0000

    Add Some Missing Flags

--- device.mk
+++ device.mk
@@ -22,5 +22,11 @@ DEVICE_PACKAGE_OVERLAYS += \
 # Include device-specific product fragments
 include $(DEVICE_PATH)/product/*.mk
 
+# TARGET_BOARD_PLATFORM specific featurization
+QCOM_BOARD_PLATFORMS := msm8937
+
+# List of targets that use video hw
+MSM_VIDC_TARGET_LIST := msm8937
+
 # Inherit proprietary files
 $(call inherit-product, vendor/xiaomi/land/land-vendor.mk)
